e9d9e462ffa0ee6eada7ec7cb46ac3dfd864a58c
Angel <miguel.loera@daltum.mx>
2022-05-11 10:11:56 -0500

    last wasn't the final

---
+++ MvvmGuia/MvvmGuia/Conexion/Cconexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+
+namespace MvvmGuia.Conexion
+{
+    internal class Cconexion
+    {
+        public static FirebaseClient firebase = new FirebaseClient("https://mvvmguia-69303-default-rtdb.firebaseio.com/");
+    }
+}
---
+++ MvvmGuia/MvvmGuia/Vistas/Pokemon/Listapokemon.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmGuia.VistaModelo.VMpokemon;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmGuia.Vistas.Pokemon
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listapokemon : ContentPage
+    {
+        public Listapokemon()
+        {
+            InitializeComponent();
+            BindingContext = new VMlistapokemon(Navigation);
+        }
+    }
+}
